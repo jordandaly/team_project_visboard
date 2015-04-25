24fc11cbd636695060c950e3c97af201bb64f3a3
JD <jordandaly@gmail.com>
2015-04-25 09:45:26 +0000

    final commit to make it up to date

--- app/models/savings_deposit.rb
+++ app/models/savings_deposit.rb
@@ -2,6 +2,6 @@ class SavingsDeposit < ActiveRecord::Base
   belongs_to :goal
   
   def self.total_on(date)
-    where("date(date) = ?", date).sum(:amount)
+    where("date(date) = ?", date).sum(:amount) 
   end
 end
